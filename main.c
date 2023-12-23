9ebfa63e647191f3cfd95cd8cd0ef7b648237f37
FattiMei <teomei68@gmail.com>
2023-12-23 11:35:31 +0100

    Added option to save report to file (it should be faster)

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <math.h>
 #include "config.h"
 #include "matrix.h"
@@ -48,8 +49,29 @@ Real e[NMAX];
 Real d[NMAX];
 
 
-int main() {
-	printf("n,residual,error\n");
+int main(int argc, char *argv[]) {
+	FILE *out = NULL;
+	bool out_needs_fclose = false;
+
+
+	if (argc == 2) {
+		const char *pathname = argv[1];
+
+		out = fopen(argv[1], "w");
+
+		if (out == NULL) {
+			fprintf(stderr, "ERROR: can't open file `%s`, quitting\n", pathname);
+			exit(EXIT_FAILURE);
+		}
+
+		out_needs_fclose = true;
+	}
+	else {
+		out = stdout;
+	}
+
+
+	fprintf(out, "n,residual,error\n");
 
 
 	for (int n = 10; n <= NMAX; ++n) {
@@ -58,7 +80,12 @@ int main() {
 		residual(b, x, r, n);
 		error(xe, x, e, n);
 
-		printf("%d,%e,%e\n", n, norm(r,n), norm(e,n));
+		fprintf(out, "%d,%e,%e\n", n, norm(r,n), norm(e,n));
+	}
+
+
+	if (out_needs_fclose) {
+		fclose(out);
 	}
 
 
