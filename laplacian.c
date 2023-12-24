4350f9d755a02ca99e0451031c3ca5237daa0839
FattiMei <teomei68@gmail.com>
2023-12-24 11:43:48 +0100

    All laplacian related functions are in the same file

--- laplacian.c
+++ laplacian.c
@@ -5,7 +5,7 @@
 Real tmp[NMAX];
 
 
-void action(const Real x[], Real y[], int n) {
+void action_naive(const Real x[], Real y[], int n) {
 	for (int i = 0; i < n; ++i) {
 		y[i] = 2.0 * x[i];
 
@@ -16,6 +16,40 @@ void action(const Real x[], Real y[], int n) {
 		if (i > 0) {
 			y[i] -= x[i-1];
 		}
+	}
+}
+
+
+void action(const Real x[], Real y[], int n) {
+	for (int i = 0; i < n; ++i) {
+		if (i == 0) {
+			y[i] = 2.0 * x[i] - x[i+1];
+		}
+		else if (i == (n-1)) {
+			y[i] = 2.0 * x[i] - x[i-1];
+		}
+		else {
+			y[i] = 2.0 * x[i] - (x[i-1] + x[i+1]);
+		}
+	}
+}
+
+
+void solve_naive(const Real b[], Real x[], int n) {
+	// eliminazione
+	tmp[0] = 2.0;
+	x[0]   = b[0];
+
+	for (int i = 1; i < n; ++i) {
+		tmp[i] = 2.0 - 1.0 / tmp[i-1];
+		x[i]   = b[i] + x[i-1] / tmp[i-1];
+	}
+
+	// sostituzione all'indietro
+	x[n-1] = x[n-1] / tmp[n-1];
+
+	for (int i = n-2; i >= 0; --i) {
+		x[i] = (x[i] + x[i+1]) / tmp[i];
 	}
 }
 
