ee00c6bcdd496d7e94f5f03265e35174e476ee74
FattiMei <teomei68@gmail.com>
2024-01-05 20:42:45 +0100

    Removed unnecessary setup when comparing different solvers

--- solving.c
+++ solving.c
@@ -14,28 +14,25 @@ int main(int argc, char *argv[]) {
 	printf("n,solve_naive,solve_better,solve_upper,solve_upper_better\n");
 
 	for (int n = 10; n <= NMAX; ++n) {
+		setup(actionf, xe, b, n);
+
 		printf("%d,", n);
 
-		setup(actionf, xe, b, n);
 		solve_naive(b, x, n);
 		residual(actionf, b, x, r, n);
 		printf("%e,", norm(r,n));
 
-		setup(actionf, xe, b, n);
 		solve_better(b, x, n);
 		residual(actionf, b, x, r, n);
 		printf("%e,", norm(r,n));
 
-		setup(actionf, xe, b, n);
 		solve_upper(b, x, n);
 		residual(actionf, b, x, r, n);
 		printf("%e,", norm(r,n));
 
-		setup(actionf, xe, b, n);
 		solve_upper_better(b, x, n);
 		residual(actionf, b, x, r, n);
 		printf("%e\n", norm(r,n));
-
 	}
 
 
