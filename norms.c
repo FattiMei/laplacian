1131914c526c1224b070836ce2bb482a7e30a9e1
FattiMei <teomei68@gmail.com>
2024-01-05 12:30:54 +0100

    Removed unused implementations for computing the L2 norm of a vector, results are fine independent of precision used

--- norms.c
+++ norms.c
@@ -3,6 +3,7 @@
 #include "norm.h"
 #include "matrix.h"
 #include "experiment.h"
+#include "laplacian.h"
 
 
 float xe[NMAX];
@@ -12,19 +13,21 @@ float r[NMAX];
 
 
 int main(int argc, char *argv[]) {
-	printf("n,normf,normd,normf_ordered\n");
+	printf("n,normf,normd\n");
 
 	for (int n = 10; n <= NMAX; ++n) {
-		setup(action, xe, b, n);
-		solve(b, x, n);
-		residual(action, b, x, r, n);
+		setup(actionf, xe, b, n);
+		solve_naive(b, x, n);
+		residual(actionf, b, x, r, n);
 
 		printf("%d,", n);
 		printf("%e,", normf(r,n));
-		printf("%e,", normd(r,n));
-		printf("%e\n", normf_ordered(r,n));
+		printf("%e\n", normd(r,n));
 	}
 
 
 	return 0;
 }
+
+
+// The results of normf and normd are practically the same
--- src/norm.c
+++ src/norm.c
@@ -3,34 +3,11 @@
 #include <stdlib.h>
 
 
-int real_comparison(const void *x, const void *y) {
-	return *((float *) x) < *((float *) y);
-}
-
-
 float normf(const float x[], int n) {
 	float sum = 0.0f;
 
 	for (int i = 0; i < n; ++i) {
 		sum += x[i] * x[i];
-	}
-
-	return sqrtf(sum);
-}
-
-
-// this function invalidates the contents of x
-float normf_ordered(float x[], int n) {
-	float sum = 0.0f;
-
-	for (int i = 0; i < n; ++i) {
-		x[i] = x[i] * x[i];
-	}
-
-	qsort(x, n, sizeof(float), real_comparison);
-
-	for (int i = 0; i < n; ++i) {
-		sum += x[i];
 	}
 
 	return sqrtf(sum);
