fcead430fee0a9f5a3d8e133586630aaf6c52cb7
FattiMei <teomei68@gmail.com>
2024-01-06 12:46:39 +0100

    Measuring the error instead of the residual gives different results!

--- solving.c
+++ solving.c
@@ -7,11 +7,11 @@
 float xe[NMAX];
 float x[NMAX];
 float b[NMAX];
-float r[NMAX];
+float e[NMAX];
 
 
 int main(int argc, char *argv[]) {
-	printf("n,solve_naive,solve_better,solve_upper,solve_upper_better\n");
+	printf("n,solve_naive,solve_better,solve_upper,solve_upper_rescaled,solve_upper_better\n");
 
 	for (int n = 10; n <= NMAX; ++n) {
 		setup(actionf, xe, b, n);
@@ -19,20 +19,24 @@ int main(int argc, char *argv[]) {
 		printf("%d,", n);
 
 		solve_naive(b, x, n);
-		residual(actionf, b, x, r, n);
-		printf("%e,", norm(r,n));
+		error(xe, x, e, n);
+		printf("%e,", norm(e,n));
 
 		solve_better(b, x, n);
-		residual(actionf, b, x, r, n);
-		printf("%e,", norm(r,n));
+		error(xe, x, e, n);
+		printf("%e,", norm(e,n));
 
 		solve_upper(b, x, n);
-		residual(actionf, b, x, r, n);
-		printf("%e,", norm(r,n));
+		error(xe, x, e, n);
+		printf("%e,", norm(e,n));
+
+		solve_upper_rescaled(b, x, n);
+		error(xe, x, e, n);
+		printf("%e,", norm(e,n));
 
 		solve_upper_better(b, x, n);
-		residual(actionf, b, x, r, n);
-		printf("%e\n", norm(r,n));
+		error(xe, x, e, n);
+		printf("%e\n", norm(e,n));
 	}
 
 
