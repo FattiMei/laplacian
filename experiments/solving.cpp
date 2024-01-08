df46f7b94a59bc17a41ede7cdd45e4efb0c4cbf5
FattiMei <teomei68@gmail.com>
2024-01-08 15:25:03 +0100

    Bugfix, there is still work to be done

--- experiments/solving.cpp
+++ experiments/solving.cpp
@@ -1,15 +1,32 @@
 #include <stdio.h>
-#include "config.h"
-#include "experiment.h"
 #include "norm.h"
 #include "laplacian.h"
+
+
+#define NMAX 10000
 
 
 float xe[NMAX];
 float x[NMAX];
 float b[NMAX];
-float r[NMAX];
 float e[NMAX];
+float diag[NMAX];
+
+
+void setup(float xe[], float b[], int n) {
+	for (int i = 0; i < n; ++i) {
+		xe[i] = 1.0;
+	}
+
+	action<double>(xe, b, n);
+}
+
+
+void error(float xe[], float x[], float e[], int n) {
+	for (int i = 0; i < n; ++i) {
+		e[i] = xe[i] - x[i];
+	}
+}
 
 
 int main(int argc, char *argv[]) {
@@ -17,21 +34,21 @@ int main(int argc, char *argv[]) {
 	printf("n,thomas(fp32),thomas(fp64),thomas(fp80)\n");
 
 	for (int n = 10; n <= NMAX; ++n) {
-		setup(actionf, xe, b, n);
+		setup(xe, b, n);
 
 		printf("%d,", n);
 
-		thomas<float>(b, x, n);
+		thomas<float>(b, diag, x, n);
 		error(xe, x, e, n);
-		printf("%e,", norm(e,n));
+		printf("%e,", norm<double>(e,n));
 
-		thomas<double>(b, x, n);
+		thomas<double>(b, diag, x, n);
 		error(xe, x, e, n);
-		printf("%e,", norm(e,n));
+		printf("%e,", norm<double>(e,n));
 
-		thomas<long double>(b, x, n);
+		thomas<long double>(b, diag, x, n);
 		error(xe, x, e, n);
-		printf("%e\n", norm(e,n));
+		printf("%e\n", norm<double>(e,n));
 	}
 
 
