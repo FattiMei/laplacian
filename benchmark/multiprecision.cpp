c48a2114dd9d29a87d6438bf02749a959b70f965
FattiMei <teomei68@gmail.com>
2024-01-09 10:49:21 +0100

    Added a benchmark for the multilevel precision norm calculation

---
+++ benchmark/multiprecision.cpp
@@ -0,0 +1,37 @@
+#include <benchmark/benchmark.h>
+#include "norm.h"
+
+
+void init(float x[], int n) {
+	for (int i = 0; i < n; ++i) {
+		x[i] = i;
+	}
+}
+
+
+static void norm_fp32(benchmark::State& state) {
+	const int n = state.range(0);
+	float *x = new float[n];
+
+	init(x, n);
+
+	for (auto _ : state) {
+		norm<float>(x,n);
+	}
+}
+
+static void norm_fp64(benchmark::State& state) {
+	const int n = state.range(0);
+	float *x = new float[n];
+
+	init(x, n);
+
+	for (auto _ : state) {
+		norm<double>(x,n);
+	}
+}
+
+
+BENCHMARK(norm_fp32)->Arg(10000);
+BENCHMARK(norm_fp64)->Arg(10000);
+BENCHMARK_MAIN();
