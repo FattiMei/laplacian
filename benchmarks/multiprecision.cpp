0fc53fc79c4fe45ca71ff676c53604b3ed71a830
FattiMeiPi <teomei68@gmail.com>
2024-01-30 20:33:48 +0100

    Refactor of multiprecision benchmark using nice functions from google benchmark

--- benchmarks/multiprecision.cpp
+++ benchmarks/multiprecision.cpp
@@ -1,58 +1,42 @@
 #include <benchmark/benchmark.h>
-#include "norm.h"
+#include "laplacian.h"
 
 
-#define NMAX 100000
+#ifndef NMAX
+#define NMAX 10000
+#endif
 
 
-float x[NMAX];
+#ifndef REAL
+#define REAL float
+#endif
 
 
-void init(float x[], int n) {
+REAL x[NMAX];
+REAL b[NMAX];
+REAL diag[NMAX];
+
+
+void init(float b[], int n) {
 	for (int i = 0; i < n; ++i) {
-		x[i] = i;
+		b[i] = i;
 	}
 }
 
 
-static void norm_fp32(benchmark::State& state) {
+template <typename DOUBLE>
+static void thomas(benchmark::State& state) {
 	const int n = state.range(0);
 
 	init(x, n);
 
 	for (auto _ : state) {
-		norm<float>(x,n);
+		thomas<DOUBLE>(b, diag, x, n);
 	}
 }
 
 
-static void norm_fp64(benchmark::State& state) {
-	const int n = state.range(0);
-
-	init(x, n);
-
-	for (auto _ : state) {
-		norm<double>(x,n);
-	}
-}
-
-
-static void norm_fp80(benchmark::State& state) {
-	const int n = state.range(0);
-
-	init(x, n);
-
-	for (auto _ : state) {
-		norm<long double>(x,n);
-	}
-}
-
-
-// How norm calculation suffers from the precision used
-BENCHMARK(norm_fp32)->Arg(NMAX);
-BENCHMARK(norm_fp64)->Arg(NMAX);
-BENCHMARK(norm_fp80)->Arg(NMAX);
+BENCHMARK(thomas<float>)->RangeMultiplier(2)->Range(10, NMAX)->Complexity();
+BENCHMARK(thomas<double>)->RangeMultiplier(2)->Range(10, NMAX)->Complexity();
 
 BENCHMARK_MAIN();
-
-
