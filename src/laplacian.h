684d41c2880f2e110c7cadf86cd5fb591c60e476
FattiMei <teomei68@gmail.com>
2024-05-13 21:30:10 +0200

    Translate comments to english

--- src/laplacian.h
+++ src/laplacian.h
@@ -55,21 +55,18 @@ template <typename DOUBLE, typename SINGLE>
 void thomas(const SINGLE b[], SINGLE diag[], SINGLE x[], int n) {
 	DOUBLE d = static_cast<DOUBLE>(2.0);
 
-
 	diag[0] = static_cast<SINGLE>(d);
 	x[0] = b[0];
 
-
 	for (int i = 1; i < n; ++i) {
-		// d = (2 * d - 1) / d con le costanti nella precisione di d;
+		// d = (2 * d - 1) / d with the constants in the same precision as d
 		d = map(d);
 
 		diag[i] = static_cast<DOUBLE>(d);
 		x[i]   = b[i] + x[i-1] / diag[i-1];
 	}
 
-
-	// sostituzione all'indietro
+	// backward substitution
 	x[n-1] = x[n-1] / diag[n-1];
 
 	for (int i = n-2; i >= 0; --i) {
@@ -82,9 +79,7 @@ template <typename DOUBLE, typename SINGLE>
 void storeless(const SINGLE b[], SINGLE diag[], SINGLE x[], int n) {
 	DOUBLE d = static_cast<DOUBLE>(2.0);
 
-
 	x[0] = b[0];
-
 
 	for (int i = 1; i < n; ++i) {
 		x[i] = b[i] + x[i-1] / d;
@@ -92,8 +87,7 @@ void storeless(const SINGLE b[], SINGLE diag[], SINGLE x[], int n) {
 		d = map(d);
 	}
 
-
-	// sostituzione all'indietro
+	// backward substitution
 	x[n-1] = x[n-1] / d;
 
 	for (int i = n-2; i >= 0; --i) {
@@ -110,9 +104,7 @@ void multifrontal(const SINGLE b[], SINGLE diag[], SINGLE x[], int n) {
 		return;
 	}
 
-
 	const int middle = n / 2;
-
 
 	#pragma omp parallel sections
 	{
@@ -122,8 +114,7 @@ void multifrontal(const SINGLE b[], SINGLE diag[], SINGLE x[], int n) {
 			diag[0]     = static_cast<SINGLE>(down);
 			x[0]        = b[0];
 
-
-			// elimino verso il basso
+			// eliminate downwards
 			for (int i = 1; i <= middle; ++i) {
 				down = map(down);
 
@@ -138,8 +129,7 @@ void multifrontal(const SINGLE b[], SINGLE diag[], SINGLE x[], int n) {
 			diag[n-1]   = static_cast<SINGLE>(up);
 			x[n-1]      = b[n-1];
 
-
-			// elimino verso l'alto
+			// eliminate upwards
 			for (int i = n-2; i > middle; --i) {
 				up = map(up);
 
@@ -149,20 +139,18 @@ void multifrontal(const SINGLE b[], SINGLE diag[], SINGLE x[], int n) {
 		}
 	}
 
-
-	// elimino la prima dalla seconda
+	// eliminate the first from the second
 	diag[middle+1] = diag[middle+1] * diag[middle] - 1.0;
 	x[middle+1] = diag[middle] * x[middle+1] + x[middle];
 
-	// l'unica equazione indipendente
+	// the only independent equation
 	x[middle+1] = x[middle+1] / diag[middle+1];
-
 
 	#pragma omp parallel sections
 	{
 		#pragma omp section
 		{
-			// sostituisco all'indietro
+			// backward substitution
 			for (int i = middle; i >= 0; --i) {
 				x[i] = (x[i] + x[i+1]) / diag[i];
 			}
@@ -170,8 +158,7 @@ void multifrontal(const SINGLE b[], SINGLE diag[], SINGLE x[], int n) {
 
 		#pragma omp section
 		{
-
-			// sostituisco in avanti
+			// forward substitution
 			for (int i = middle+2; i < n; ++i) {
 				x[i] = (x[i] + x[i-1]) / diag[i];
 			}
@@ -187,10 +174,8 @@ void simultaneous(const SINGLE b[], SINGLE diag[], SINGLE x[], int n) {
 		return;
 	}
 
-
 	const int middle = n / 2;
 	DOUBLE down, up;
-
 
 	#pragma omp parallel sections
 	{
@@ -200,8 +185,6 @@ void simultaneous(const SINGLE b[], SINGLE diag[], SINGLE x[], int n) {
 			diag[0]     = static_cast<SINGLE>(down);
 			x[0]        = b[0];
 
-
-			// elimino verso il basso
 			for (int i = 1; i <= middle; ++i) {
 				down = map(down);
 
@@ -216,8 +199,6 @@ void simultaneous(const SINGLE b[], SINGLE diag[], SINGLE x[], int n) {
 			diag[n-1]   = static_cast<SINGLE>(up);
 			x[n-1]      = b[n-1];
 
-
-			// elimino verso l'alto
 			for (int i = n-2; i > middle; --i) {
 				up = map(up);
 
@@ -228,14 +209,12 @@ void simultaneous(const SINGLE b[], SINGLE diag[], SINGLE x[], int n) {
 	}
 
 
-	// eliminazione simultanea, in entrambi i sensi
+	// simultaneous elimination
 	SINGLE tmp = x[middle+1];
 
-	// elimino verso il basso
 	diag[middle+1] = static_cast<SINGLE>(up - static_cast<DOUBLE>(1.0) / down);
 	x[middle+1] = x[middle+1] + x[middle] / static_cast<SINGLE>(down);
 
-	// elimino verso l'alto
 	diag[middle] = static_cast<SINGLE>(down - static_cast<DOUBLE>(1.0) / up);
 	x[middle] = x[middle] + tmp / static_cast<SINGLE>(up);
 
@@ -244,7 +223,6 @@ void simultaneous(const SINGLE b[], SINGLE diag[], SINGLE x[], int n) {
 	{
 		#pragma omp section
 		{
-			// sostituisco all'indietro
 			x[middle] = x[middle] / diag[middle];
 
 			for (int i = middle-1; i >= 0; --i) {
@@ -254,8 +232,6 @@ void simultaneous(const SINGLE b[], SINGLE diag[], SINGLE x[], int n) {
 
 		#pragma omp section
 		{
-
-			// sostituisco in avanti
 			x[middle+1] = x[middle+1] / diag[middle+1];
 
 			for (int i = middle+2; i < n; ++i) {
